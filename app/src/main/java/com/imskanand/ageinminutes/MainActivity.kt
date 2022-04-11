31189ae45f2eb59b19ab3e52d6e29853e216d11a
imskanand <the.anand3099@gmail.com>
2022-04-12 01:52:06 +0530

    Implemented the button click for Date Picker Dialogue

--- app/src/main/java/com/imskanand/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/imskanand/ageinminutes/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.imskanand.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
+        btnDatePicker.setOnClickListener {view->
+            clickDatePicker(view)
+            Toast.makeText(this, "Button Clicked",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun clickDatePicker(view: View){
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{
+                    view, year, month, dayOfMonth->}
+            ,year
+            ,month
+            ,day).show()
     }
 }
