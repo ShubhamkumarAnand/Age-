c0fdbc27c3e34c9684980b650615a45c32be284d
imskanand <the.anand3099@gmail.com>
2022-04-12 02:13:09 +0530

    Date Picker Parsed

--- app/src/main/java/com/imskanand/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/imskanand/ageinminutes/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.imskanand.ageinminutes
 
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,23 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
-        btnDatePicker.setOnClickListener {view->
+        btnDatePicker.setOnClickListener { view ->
             clickDatePicker(view)
-            Toast.makeText(this, "Button Clicked",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "DatePicker Clicked", Toast.LENGTH_LONG).show()
         }
     }
 
-    fun clickDatePicker(view: View){
+    fun clickDatePicker(view: View) {
         val myCalender = Calendar.getInstance()
         val year = myCalender.get(Calendar.YEAR)
         val month = myCalender.get(Calendar.MONTH)
         val day = myCalender.get(Calendar.DAY_OF_MONTH)
 
         DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{
-                    view, year, month, dayOfMonth->}
-            ,year
-            ,month
-            ,day).show()
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this,"The Chosen Year is $selectedYear, Month is $selectedMonth, Day is $selectedDayOfMonth",
+                    Toast.LENGTH_LONG).show()
+                val selectedDate = "$selectedYear/${selectedMonth+1}/$selectedDayOfMonth"
+                var tvSelectedDate = findViewById<TextView>(R.id.tvSelectedDate)
+                tvSelectedDate.text = selectedDate
+                val sdf = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH)
+                val date = sdf.parse(selectedDate)
+            },
+            year,
+            month,
+            day).show()
     }
 }
